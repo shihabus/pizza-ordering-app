060c41fadba34a0f39a183e684ffdc251923e6b6
Shihabudheen US <shihabudheen@dunzo.in>
2021-08-01 14:08:01 +0530

    adding TimeLine

---
+++ elements/shared/TimeLine/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { Wrapper, TimeLineContainer, Dot, TextContainer } from "./styles";
+
+const TimeLine = ({ children, currentActiveStep, ...props }) => (
+  <Wrapper {...props}>
+    {React.Children.map(children, (child, idx) => {
+      return React.cloneElement(child, {
+        step: idx + 1,
+        currentActiveStep,
+        totalSteps: children?.length || 0,
+      });
+    })}
+  </Wrapper>
+);
+
+const TimelineItem = ({
+  children,
+  currentActiveStep,
+  totalSteps,
+  step,
+  ...rest
+}) => {
+  const isDone = step < currentActiveStep;
+  const isActive = step === currentActiveStep;
+  const isLastStep = step === totalSteps;
+  return (
+    <TimeLineContainer showLine={!isLastStep} isDone={isDone}>
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, {
+          isDone,
+          isLastStep,
+          isActive,
+          step,
+          ...rest,
+        })
+      )}
+    </TimeLineContainer>
+  );
+};
+
+const TimelineDot = ({ children, isDone, isActive }) => {
+  return <Dot>{children({ isDone, isActive })}</Dot>;
+};
+
+TimeLine.Item = TimelineItem;
+TimeLine.Dot = TimelineDot;
+TimeLine.Content = TextContainer;
+
+export default TimeLine;
---
+++ elements/shared/TimeLine/styles.js
@@ -0,0 +1,50 @@
+import styled, { css } from "styled-components";
+
+import Colors from "elements/shared/Colors";
+import { inlineStyles } from "Utils/stylesUtil";
+
+const Wrapper = styled.ul`
+  padding: 40px;
+  list-style: none;
+  ${inlineStyles}
+`;
+
+const TimeLineContainer = styled.li`
+  position: relative;
+  padding-bottom: 10px;
+
+  ${({ showLine }) =>
+    showLine &&
+    css`
+      &::before {
+        content: "";
+        display: block;
+        height: 100%;
+        position: absolute;
+        width: 0.46px;
+        background-color: ${Colors.stroke};
+        ${({ isDone }) =>
+          isDone &&
+          css`
+            width: 1px;
+            background-color: ${Colors.red};
+          `}
+      }
+      padding-bottom: 34px;
+    `}
+`;
+
+const Dot = styled.div`
+  position: absolute;
+  transform: translate(-50%, -25%);
+`;
+
+const TextContainer = styled.div`
+  max-height: 30px;
+  padding-left: 46px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export { Wrapper, TimeLineContainer, Dot, TextContainer };
