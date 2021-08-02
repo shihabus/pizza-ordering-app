70933f5a7bcad99b0f06c02b2db42fc7bc388e70
Shihabudheen US <shihabudheen@dunzo.in>
2021-08-02 19:17:06 +0530

    dummy index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { routeConstants } from "constants/routeConstants";
+import { useRouter } from "node_modules/next/dist/client/router";
+
 export default function Home() {
+  const router = useRouter();
   return (
     <>
       <div>Home</div>
+      <button onClick={() => router.push(routeConstants.chooseSize)}>
+        Root
+      </button>
     </>
   );
 }
